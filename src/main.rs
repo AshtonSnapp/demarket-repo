238e24f9dc7da48ab88bbe612742f43f193ef205
Ashton Snapp <snappgamez@outlook.com>
2022-01-11 08:57:52 -0600

    Made placeholder Rocket app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "This is a test! I'll work on getting an actual frontend set up once I figure out how to deliver HTML pages..."
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
