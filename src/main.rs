fc2122faeadd364ae940140c54665bc6b475ce1e
Ashton Snapp <snappgamez@outlook.com>
2022-01-17 15:35:39 -0600

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::{relative, FileServer};
+use rocket::serde::{json::*};
 
 #[get("/")]
-fn index() -> &'static str {
-    "This is a test! I'll work on getting an actual frontend set up once I figure out how to deliver HTML pages..."
+fn api() -> Value {
+    json!({
+        "test": true
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", FileServer::from(relative!("/frontend/public")))
+        .mount("/api", routes![api])
 }
